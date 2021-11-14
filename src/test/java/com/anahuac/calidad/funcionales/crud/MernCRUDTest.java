5e4d3981ee445a4dca2a23a2dbdecac570334dbb
wariormex <wariormex@hotmail.com>
2021-11-13 20:39:43 -0600

    MERN CRUD Online Terminado

--- src/test/java/com/anahuac/calidad/funcionales/crud/MernCRUDTest.java
+++ src/test/java/com/anahuac/calidad/funcionales/crud/MernCRUDTest.java
@@ -27,29 +27,124 @@ public class MernCRUDTest {
 	    baseUrl = "https://mern-crud.herokuapp.com/";
 	    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 	    js = (JavascriptExecutor) driver;
+	    
+	    driver.get("https://mern-crud.herokuapp.com/");
+	    String tag = driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody")).getText();
+	    while(tag != "") {
+	    	pause(1000);
+	    	driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
+		    driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/button[1]")).click();
+		    pause(1000);
+		    tag = driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody")).getText();
+	    } 	
 	  }
-	  
+
 	  @Test
 	  public void testCreateUser() throws Exception {
 	    driver.get("https://mern-crud.herokuapp.com/");
 	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();//"//div[@id='root']/div/div[2]/button"
 	    driver.findElement(By.name("name")).click();
 	    driver.findElement(By.name("name")).clear();
-	    driver.findElement(By.name("name")).sendKeys("userTest1");
+	    driver.findElement(By.name("name")).sendKeys("userTestCreate");
 	    driver.findElement(By.name("email")).click();
 	    driver.findElement(By.name("email")).clear();
-	    driver.findElement(By.name("email")).sendKeys("userTest1@gmail.com");
+	    driver.findElement(By.name("email")).sendKeys("userTestCreate@gmail.com");
 	    driver.findElement(By.name("age")).click();
 	    driver.findElement(By.name("age")).clear();
 	    driver.findElement(By.name("age")).sendKeys("20");
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Female'])[1]/following::span[1]")).click();
 	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
-	    pause(5000);
+	    pause(1000);
 	    String tag = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/div")).getText();
 	    assertThat("Nice one!", is(tag));
 	  }
+	  
+	  @Test
+	  public void testRetrieveUser() throws Exception {
+	    driver.get("https://mern-crud.herokuapp.com/");
+	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+	    driver.findElement(By.name("name")).click();
+	    driver.findElement(By.name("name")).clear();
+	    driver.findElement(By.name("name")).sendKeys("userTestRetrieve");
+	    driver.findElement(By.name("email")).click();
+	    driver.findElement(By.name("email")).clear();
+	    driver.findElement(By.name("email")).sendKeys("userTestRetrieve@gmail.com");
+	    driver.findElement(By.name("age")).click();
+	    driver.findElement(By.name("age")).clear();
+	    driver.findElement(By.name("age")).sendKeys("20");
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Female'])[1]/following::span[1]")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Delete'])[1]/following::div[1]")).click();
+	    pause(1000);
+	    String tag = driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody/tr/td[1]")).getText();
+	    assertThat("Usertestretrieve", is(tag));
+	  }
 
+	  @Test
+	  public void testUpdateUser() throws Exception {
+		 driver.get("https://mern-crud.herokuapp.com/");
+		 driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+		 driver.findElement(By.name("name")).click();
+		 driver.findElement(By.name("name")).clear();
+		 driver.findElement(By.name("name")).sendKeys("userTestUpdate");
+		 driver.findElement(By.name("email")).click();
+		 driver.findElement(By.name("email")).clear();
+		 driver.findElement(By.name("email")).sendKeys("userTestUpdate@gmail.com");
+		 driver.findElement(By.name("age")).click();
+		 driver.findElement(By.name("age")).clear();
+		 driver.findElement(By.name("age")).sendKeys("20");
+		 driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+		 driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Female'])[1]/following::span[1]")).click();
+		 driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+		 driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Delete'])[1]/following::div[1]")).click();
+		 pause(1000);
+		 driver.findElement(By.xpath("/html/body/div[2]/div/i")).click();
+		 pause(1000);
+		 driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
+		 driver.findElement(By.name("name")).click();
+		 driver.findElement(By.name("name")).clear();
+		 driver.findElement(By.name("name")).sendKeys("Usertestupdated");
+		 driver.findElement(By.name("email")).click();
+		 driver.findElement(By.name("email")).clear();
+		 driver.findElement(By.name("email")).sendKeys("usertestupdated@gmail.com");
+		 driver.findElement(By.name("age")).click();
+		 driver.findElement(By.name("age")).clear();
+		 driver.findElement(By.name("age")).sendKeys("21");
+		 driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+		 driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
+		 driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+		 pause(1000);
+		 String tag = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p")).getText();
+		 assertThat("Successfully updated!", is(tag));
+	  }
+	    
+	  @Test
+	  public void testDeleteUser() throws Exception {
+	    driver.get("https://mern-crud.herokuapp.com/");
+	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+	    driver.findElement(By.name("name")).click();
+	    driver.findElement(By.name("name")).clear();
+	    driver.findElement(By.name("name")).sendKeys("userTestDelete");
+	    driver.findElement(By.name("email")).click();
+	    driver.findElement(By.name("email")).clear();
+	    driver.findElement(By.name("email")).sendKeys("userTestDelete@gmail.com");
+	    driver.findElement(By.name("age")).click();
+	    driver.findElement(By.name("age")).clear();
+	    driver.findElement(By.name("age")).sendKeys("20");
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Female'])[1]/following::span[1]")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+	    driver.findElement(By.xpath("/html/body/div[2]/div/i")).click();
+	    pause(1000);
+	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
+	    driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/button[1]")).click();
+	    pause(1000);
+	    String tag = driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody")).getText();
+	    assertThat("", is(tag));
+	  }
+	  
 	  @After
 	  public void tearDown() throws Exception {
 	    driver.quit();
